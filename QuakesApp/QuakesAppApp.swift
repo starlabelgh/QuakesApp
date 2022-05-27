0b6c12be51e01cea5713a521fec7558f6722674a
starlabelgh <hello@starlabel.co>
2022-05-27 10:40:19 +0000

    Initial Commit

---
+++ QuakesApp/QuakesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuakesAppApp.swift
+//  QuakesApp
+//
+//  Created by Moses Samanhyia on 27/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct QuakesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
